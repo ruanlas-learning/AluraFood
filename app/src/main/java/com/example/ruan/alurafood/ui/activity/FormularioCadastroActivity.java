b09b9112dfd882b4c9164ac7b612f0e987301d66
Ruan Silva <ruanluiz.as@gmail.com>
2019-01-14 22:16:15 -0200

    aula 01

---
+++ app/src/main/java/com/example/ruan/alurafood/ui/activity/FormularioCadastroActivity.java
@@ -0,0 +1,15 @@
+package com.example.ruan.alurafood.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ruan.alurafood.R;
+
+public class FormularioCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_cadastro);
+    }
+}
